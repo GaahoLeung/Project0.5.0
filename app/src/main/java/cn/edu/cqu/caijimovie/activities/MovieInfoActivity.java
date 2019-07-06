6bbf9381db54538da751b7d38cdedc9520af3f44
GaahoLeung <411973089@qq.com>
2019-07-06 12:07:28 +0800

    修改了MovieInfoActivity里的各种布局
    Signed-off-by: GaahoLeung <411973089@qq.com>

--- app/src/main/java/cn/edu/cqu/caijimovie/activities/MovieInfoActivity.java
+++ app/src/main/java/cn/edu/cqu/caijimovie/activities/MovieInfoActivity.java
@@ -6,20 +6,37 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 import cn.edu.cqu.caijimovie.R;
+import cn.edu.cqu.caijimovie.entities.Movie;
 
 public class MovieInfoActivity extends AppCompatActivity {
-
+    Movie mMovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
+        mMovie = (Movie) getIntent().getSerializableExtra("onClickMovie");
         setTitle(null);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+        });
+        ImageButton imageButtonBack = findViewById(R.id.movie_info_back);
+        imageButtonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab_like);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
