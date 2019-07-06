dae9e3bd9b2ede6bed3d7c7669967bad27f06098
GaahoLeung <411973089@qq.com>
2019-07-06 15:11:05 +0800

    初步指示MovieInfoActivity里的逻辑
    Signed-off-by: GaahoLeung <411973089@qq.com>

--- app/src/main/java/cn/edu/cqu/caijimovie/activities/MovieInfoActivity.java
+++ app/src/main/java/cn/edu/cqu/caijimovie/activities/MovieInfoActivity.java
@@ -1,8 +1,11 @@
 package cn.edu.cqu.caijimovie.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,20 +16,40 @@ import cn.edu.cqu.caijimovie.entities.Movie;
 
 public class MovieInfoActivity extends AppCompatActivity {
     Movie mMovie;
+    AppBarLayout mAppBarLayout;
+    NestedScrollView mNestedScrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
+        mAppBarLayout = findViewById(R.id.app_bar);
+        mNestedScrollView = findViewById(R.id.include);
         mMovie = (Movie) getIntent().getSerializableExtra("onClickMovie");
         setTitle(null);
+
+        //Todo:ToolBar的返回顶部
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
+                CoordinatorLayout.Behavior behavior =
+                        ((CoordinatorLayout.LayoutParams) mAppBarLayout.getLayoutParams()).getBehavior();
+                if (behavior instanceof AppBarLayout.Behavior) {
+                    AppBarLayout.Behavior appBarLayoutBehavior = (AppBarLayout.Behavior) behavior;
+                    int topAndBottomOffset = appBarLayoutBehavior.getTopAndBottomOffset();
+                    if (topAndBottomOffset != 0) {
+                        appBarLayoutBehavior.setTopAndBottomOffset(0);
+                        mNestedScrollView.fling(0);
+                        mNestedScrollView.scrollTo(0, 0);
+                    }
+                }
+                mAppBarLayout.setExpanded(true);
+
             }
         });
+        setSupportActionBar(toolbar);
+
+        //ToolBar返回按钮
         ImageButton imageButtonBack = findViewById(R.id.movie_info_back);
         imageButtonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,15 +57,28 @@ public class MovieInfoActivity extends AppCompatActivity {
                 finish();
             }
         });
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab_like);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //Todo:通过UID和MID来设置Like图标样式,并通过http来上传
+        FloatingActionButton fabLike = findViewById(R.id.fab_like);
+
+        fabLike.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        //Todo:进入评论页面,如果评论过....就先算了
+        FloatingActionButton fabComment = findViewById(R.id.fab_comment);
+        fabComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
